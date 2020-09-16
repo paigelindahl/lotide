249c87e25dbd00f6375a4f743e5dbf4170d826f5
paigelindahl <paigemarlindahl@gmail.com>
2020-09-16 23:01:18 +0000

    updated

--- countLetters.js
+++ countLetters.js
@@ -1,21 +1,27 @@
-/*const assertEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
-};*/
+};
+
 
 const countLetters = function (str) {
   let finalResult = {};
   for (let letters of str){
+    if(letters === ' ') {
+      continue;
+    } 
     if(finalResult[letters]) {
       finalResult[letters] += 1;
     } else {
       finalResult[letters] = 1;
     }
   }
+  console.log(finalResult);
   return finalResult;
-}
+};
 
-console.log(countLetters('lighthouse'));
+const arg = { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 };
+assertEqual(countLetters('lighthouse in the house'), arg);
