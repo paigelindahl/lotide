620c0f4601ec7d5d9dc6b35ccb09bad422e91ec1
paigelindahl <paigemarlindahl@gmail.com>
2020-09-16 01:35:05 +0000

    updated

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+
+const eqArrays = function (arr1, arr2) {
+  if(arr1.length != arr2.length){
+    return false;
+  } 
+  for(let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    } 
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log('match');
+  } else {
+    console.log('no match');
+  }
+}
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
