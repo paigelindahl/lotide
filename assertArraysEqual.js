e9d9c708cdfdfa4ab57203c9e4c441e83e2abc1b
paigelindahl <paigemarlindahl@gmail.com>
2020-09-21 20:18:09 +0000

    updated assertion message

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,9 +4,9 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log('Assertion passed. You have a match.');
+    console.log('✅✅✅Assertion passed. You have a match.');
   } else {
-    console.log('Assertion failed. No match.');
+    console.log('🛑🛑🛑Assertion failed. No match.');
   }
 };
 
