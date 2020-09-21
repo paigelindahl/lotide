4483aef3d250a27552a4c2631f7283b628dd8c78
paigelindahl <paigemarlindahl@gmail.com>
2020-09-21 22:42:18 +0000

    added export module

--- countLetters.js
+++ countLetters.js
@@ -1,3 +1,5 @@
+const countOnly = require("./countOnly");
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -25,3 +27,5 @@ const countLetters = function (str) {
 
 const arg = { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 };
 assertEqual(countLetters('lighthouse in the house'), arg);
+
+module.exports = countLetters;
