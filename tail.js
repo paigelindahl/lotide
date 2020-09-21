477aee2bab5dd6b26fa4d4d27ffe82d3e1deb253
paigelindahl <paigemarlindahl@gmail.com>
2020-09-21 16:06:16 +0000

    removed test code into seperate folder

--- tail.js
+++ tail.js
@@ -1,15 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//tail function looks at an array without its head element and compares
+// the length to another value assertEqual function
+
+const assertEqual = require('./assertEqual')
 
 const tail = function(arr) {
   let newArray = arr.slice(1, arr.length);
   return newArray.length;
 };
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), 2);
+module.exports = tail;
+
+
 
