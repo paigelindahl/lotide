afeb62df7fb95a211b70eac6f53eedf1f63a6d94
paigelindahl <paigemarlindahl@gmail.com>
2020-09-21 21:30:52 +0000

    added chai and mocha testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,16 @@
 // tests my tail function
 
 const tail = require('../tail.js');
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), 2);
-assertEqual(tail(["Hello", "this", "is", "a", "test"]), 4);
+describe("#tail", () => {
+  it("returns '2' for ['Hello', 'Lighthouse', Labs']", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]), 2);
+  });
+  it("returns '4' for ['Hello', 'this', 'is', 'a', 'test']", () => {
+    assert.strictEqual(tail(["Hello", "this", "is", "a", "test"]), 4);
+  });
+});
+
+
