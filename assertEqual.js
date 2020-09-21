616741d7c5fa858723c4ec7400a8b99592b6776d
paigelindahl <paigemarlindahl@gmail.com>
2020-09-21 15:43:29 +0000

    separated a test file

--- assertEqual.js
+++ assertEqual.js
@@ -8,7 +8,6 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual('hi', 'hi');
-assertEqual(1, 2);
+
+
+module.exports = assertEqual;
