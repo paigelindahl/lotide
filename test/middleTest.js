d4df5476897a5105642cf2a3299d1f34c707f953
paigelindahl <paigemarlindahl@gmail.com>
2020-09-21 20:27:12 +0000

    set up test file for middle.js

---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+//test file for middle.js
+
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+assertArraysEqual(middle(["1", "2"]), ["3", "4"]);
+assertArraysEqual(middle(["1", "2", "3", "4", "5"]), ["3"]);
+assertArraysEqual(middle(["1", "2", "3", "4", "5", "6"]), ["3", "4"]);
+
