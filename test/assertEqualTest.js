31f907333a46f74045d48bd1181afbe9de071e5b
paigelindahl <paigemarlindahl@gmail.com>
2020-09-21 15:46:13 +0000

    created seperate test file for assertEqual function

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+//tests our assertEqual function
+
+const assertEqual = require('../assertEqual');
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual('hi', 'hi');
+assertEqual(1, 2);
