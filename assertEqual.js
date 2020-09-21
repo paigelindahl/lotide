691e7a17cb9380f30f2c409947e7b8cd0a895779
paigelindahl <paigemarlindahl@gmail.com>
2020-09-21 14:53:10 +0000

    added function definition in comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,6 @@
+//function compares two values to determine if the actual value 
+// equates to the expected value
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
